05c678c094962fec4232ec8d7bb804832e2dff92
MAMUER <101267610+MAMUER@users.noreply.github.com>
2023-09-01 18:05:34 +0300

    Update Source.cpp

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,35 +1,33 @@
-#include <windows.h> //чтобы работать с графикой
+#include <windows.h> 
 #include < conio.h >
-#include <iostream>  //библиотека ввода вывода
+#include <iostream>  
 using namespace std;
 int main() {
-	double k = 0.1;
+	double k = (double)1/10;
 	int M = 3;
 	int J = 1;
 	double F;
 	double W;
-	HWND myconsole = GetConsoleWindow(); //HWND - класс окна консоли   myconsole - имя переменной   GetConsoleWindow() - извлекает маркер окна, используемый консолью, связанной с вызывающим процессом
-
-	HDC mydc = GetDC(myconsole); //HDC - обработчик контекста устройства, использующийся для рисования в клиентской области окна  mydc - имя переменной  GetDC() - возвращает контекст устройства окна
-	
-	COLORREF COLOR1 = RGB(255, 0, 0);
+	HWND myconsole = GetConsoleWindow(); 
+	HDC mydc = GetDC(myconsole); 
+	COLORREF COLOR1 = RGB(255, 0, 255);
 	COLORREF COLOR2 = RGB(0, 255, 0);
-	for (int t = 0; t <=1000; t++) {
+	COLORREF COLOR3 = RGB(255, 255, 255);
+	for (int t = 0; t <= 1000; t++) {
 		if (t * k < M) {
 			F = 0;
 			W = 0;
 		}
-		else {
-			F = ((k * t - M) * t * t) / (J * 2);
-			W = F / t;
+		else {			
+			W = (k * t - M) * t / J;
+			F = (W * t) / 2;
 		}
-		for (int x = 500; x <= 1000; x++) {
-			SetPixel(mydc, t, F, COLOR1);
-			SetPixel(mydc, t, W, COLOR2);
-		}
+		SetPixel(mydc, t+400, -F + 400, COLOR1);
+		SetPixel(mydc, t+400, -W + 400, COLOR2);
+		SetPixel(mydc, t, 400, COLOR3);
+		SetPixel(mydc, 400, t, COLOR3);
 	}
-	_getch(); //возвращает значение переменной (изображение после напечатывания не закроется)
-
+	_getch(); 
 	ReleaseDC(myconsole, mydc);
 	return 0;
 }
